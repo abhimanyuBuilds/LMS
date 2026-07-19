40a1c47a02cdb68cbd61cce8a63db9287ac9954a
Abhimanyu Singh <158279512+abhimanyuBuilds@users.noreply.github.com>
2026-07-19 15:47:06 +0530

    Add new features

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -64,7 +64,6 @@ export const createNewCourse = catchAsync(async (req, res) => {
  * @route GET /api/v1/courses/search
  */
 
-
 export const searchCourses = catchAsync(async (req, res) => {
   const { search = "", categories = [], level, priceRange, sortBy = "newest" } = req.query
 
@@ -136,23 +135,112 @@ export const searchCourses = catchAsync(async (req, res) => {
  * @route GET /api/v1/courses/published
  */
 export const getPublishedCourses = catchAsync(async (req, res) => {
-  
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit
+
+  const [courses, total] = await Promise.all([
+    Course.find({ isPublished: true })
+      .populate({
+        path: "instructor",
+        select: "name avatar"
+      })
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit),
+    Course.countDocuments({ isPublished: true })
+  ]);
+
+
+  return res
+    .status(200)
+    .json({
+      sucess: true,
+      data: courses,
+      pagination: {
+        page,
+        limit,
+        total,
+        pages: Math.ceil(total / limit),
+      },
+    });
 });
+
 
 /**
  * Get courses created by the current user
  * @route GET /api/v1/courses/my-courses
  */
+
 export const getMyCreatedCourses = catchAsync(async (req, res) => {
-  
+  const courses = await Course.find({ instructor: req.id }).populate({
+    path: "enrolledStudents",
+    select: "name avatar",
+  });
+
+  return res
+    .status(200)
+    .json({
+      success: true,
+      count: courses.length,
+      data: courses,
+    });
 });
+
 
 /**
  * Update course details
  * @route PATCH /api/v1/courses/:courseId
  */
 export const updateCourseDetails = catchAsync(async (req, res) => {
-  
+  const { courseId } = req.params;
+  const { title, subtitle, description, category, level, price } = req.body;
+
+  const course = await Course.findById(courseId);
+  if (!course) {
+    throw new ApiError(404, "Course not found")
+  }
+
+  // verify ownerShip 
+
+  if (!course.instructor.toString() !== req.id) {
+    throw new ApiError("Not Authorized to update this course")
+  };
+
+  //handle thumbnail upload
+  let thumbnail;
+  if (req.file) {
+    if (course.thumbnail) {
+      await deleteMediaFromCloudinary(course.thumbnail);
+    }
+
+    const result = await uploadOnCLoudinary(req.file.path);
+    thumbnail = result?.secure_url || req.file.path;
+  };
+
+  const updateCourse = await Course.findByIdAndUpdate(
+    courseId,
+    {
+      title,
+      subtitle,
+      description,
+      category,
+      level,
+      price,
+      ...(thumbnail && { thumbnail }),
+    },
+    {
+      new: true, runValidators: true
+    }
+  );
+
+  return res
+    .status(200)
+    .json({
+      success: true,
+      message: "course updated successfully",
+      data: updateCourse
+    })
 });
 
 /**
@@ -160,7 +248,30 @@ export const updateCourseDetails = catchAsync(async (req, res) => {
  * @route GET /api/v1/courses/:courseId
  */
 export const getCourseDetails = catchAsync(async (req, res) => {
- 
+
+  const course = await Course.findById(req.params.courseId)
+    .populate({
+      path: "instructor",
+      select: "name avatar bio"
+    })
+    .populate({
+      path: "lectures",
+      select: "title  subtitle description isPreview order"
+    });
+
+  if (!course) {
+    throw new ApiError(404, "Course not found")
+  };
+
+  return res
+    .status(200)
+    .json({
+      success: true,
+      data: {
+        ...course.toString(),
+        averageRating: course.averageRating,
+      },
+    });
 });
 
 /**
@@ -168,7 +279,52 @@ export const getCourseDetails = catchAsync(async (req, res) => {
  * @route POST /api/v1/courses/:courseId/lectures
  */
 export const addLectureToCourse = catchAsync(async (req, res) => {
- 
+  const { title, description, isPreview } = req.body;
+  const { courseId } = req.params
+
+  const course = await Course.findById(courseId);
+
+  if (!course) {
+    throw new ApiError(404, "Corse not found")
+  };
+
+  if (course.instructor.toString() !== req.id) {
+    throw new ApiError(403, "Not Authorized to update this course..")
+  };
+
+  if (!req.file) {
+    throw new ApiError(400, "Videl file is required")
+  };
+
+  // upload to cloudinary
+
+  const result = await uploadOnCLoudinary(req.file.path)
+  if (!result) {
+    throw new ApiError(400, "Error uploading video")
+  };
+
+  const lecture = await Lecture.create({
+    title,
+    description,
+    isPreview,
+    order: course.leactures.length + 1,
+    videoUrl: result?.secure_url || req.file.path,
+    publicId: result?.secure_url || req.file.path,
+    duration: result?.duration || 0,
+  });
+
+  // add lecture to course 
+
+  course.lectures.push(lecture._id);
+  await courses.save()
+
+  return res
+    .status(200)
+    .json({
+      success: true,
+      message: "Leacture added successfully",
+      data: lecture,
+    });
 });
 
 /**
@@ -176,4 +332,32 @@ export const addLectureToCourse = catchAsync(async (req, res) => {
  * @route GET /api/v1/courses/:courseId/lectures
  */
 export const getCourseLectures = catchAsync(async (req, res) => {
+  const course = await Course.findById(req.params, courseId).populate({
+    path: "lectures",
+    select: "title description videoUrl duration isPreview order",
+    options: { sort: { order: 1 } }
+  });
+
+  if (!course) {
+    throw new ApiError(404, "course not found")
+  };
+
+  const isEnrolled = course.enrolledStudents.includes(req.id);
+  const isInstructor = course.instructor.toString() === req.id;
+
+  let lectures = course.lectures;
+  if (!isEnrolled && !isInstructor) {
+    lectures = lectures.filter((lecture) => lecture.isPreview)
+  };
+
+  return res
+    .status(200)
+    .json({
+      sucess: true,
+      data: {
+        lectures,
+        isEnrolled,
+        isInstructor
+      }
+    })
 });
