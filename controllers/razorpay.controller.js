8fa9d9d4d4ff7e64997652a5504dfff432a3677b
Abhimanyu Singh <158279512+abhimanyuBuilds@users.noreply.github.com>
2026-08-11 17:53:11 +0530

    owned the seprate logger file for payment methods

--- controllers/razorpay.controller.js
+++ controllers/razorpay.controller.js
@@ -4,8 +4,9 @@ import Razorpay from "razorpay";
 import crypto from "crypto";
 import { Course } from "../models/course.model.js";
 import { CoursePurchase } from "../models/coursePurchase.model.js";
-import { PurchasedCourseMailgenContent , sendEmail } from "../services/sendMail.js"
+import { PurchasedCourseMailgenContent, sendEmail } from "../services/sendMail.js"
 import { ApiError } from "../utils/ApiError.js";
+import { logPayments } from "../services/payment.logger.js";
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
@@ -19,14 +20,10 @@ export const createRazorpayOrder = async (req, res) => {
     const userId = req.user.id;
     const { courseId } = req.body;
 
-
-
-
     const course = await Course.findById(courseId);
     if (!course) {
       throw new ApiError(404, "Course not found..")
     };
-
 
     // Now create a new purchase course after validate that course is their........
 
@@ -38,7 +35,6 @@ export const createRazorpayOrder = async (req, res) => {
       status: "pending",
       paymentMethod: "razorpay",
     });
-
 
     // create razorpay order
 
@@ -52,8 +48,19 @@ export const createRazorpayOrder = async (req, res) => {
       },
     };
 
-
     const order = await razorpay.orders.create(options);
+
+    logPayments({
+      userId: req.user.id,
+      courseId: course.courseId,
+      orderId: order.id,
+      paymentId: null,
+      amount: course.price,
+      currency: "INR",
+      status: "pending",
+      verificationTime: Date.now(),
+      failureReason: null
+    });
 
     // storing razorpay order id
     newCoursePurchase.paymentId = order.id;
@@ -70,6 +77,7 @@ export const createRazorpayOrder = async (req, res) => {
         image: course.thumbnail,
       }
     });
+
   } catch (error) {
     console.error("Error creating RazorPay order.", error);
     res
@@ -80,12 +88,17 @@ export const createRazorpayOrder = async (req, res) => {
   }
 };
 
+
+
+
+
 export const verifyPayment = async (req, res) => {
   try {
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
       req.body;
 
- if (
+
+    if (
       !razorpay_order_id ||
       !razorpay_payment_id ||
       !razorpay_signature
@@ -94,68 +107,123 @@ export const verifyPayment = async (req, res) => {
         400,
         "razorpay_order_id, razorpay_payment_id and razorpay_signature are required"
       );
+
     }
     // verify payment signature
 
+
+
     const body = razorpay_order_id + "|" + razorpay_payment_id;
+
     const expectedSignature = crypto
       .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
       .update(body.toString())
       .digest('hex');
 
-    const isAuthentic = expectedSignature === razorpay_signature;
 
-/**Debug
-  console.log("========== RAZORPAY VERIFY ==========");
-console.log("Order ID:", razorpay_order_id);
-console.log("Payment ID:", razorpay_payment_id);
-console.log("Received Signature:", razorpay_signature);
-console.log("Expected Signature:", expectedSignature);
-console.log("Secret exists:", !!process.env.RAZORPAY_KEY_SECRET);
-console.log("======================================");
-*/  
+
+    const isAuthentic = expectedSignature === razorpay_signature;
 
 
     if (!isAuthentic) {
-      throw new ApiError(404, "Payment verification failed..")
-    };
+
+      logPayments({
+        userId: req.user.id,
+        courseId: null,
+        orderId: razorpay_order_id,
+        paymentId: razorpay_payment_id,
+        amount: null,
+        currency: "INR",
+        status: "failed",
+        verificationTime: new Date.now().toISOString(),
+        failureReason: "Invalid RazorPay signature"
+      });
+
+      throw new ApiError(400,
+        "Payment verification failed.."
+      );
+
+    }
+
+
+    /**Debug
+      console.log("========== RAZORPAY VERIFY ==========");
+    console.log("Order ID:", razorpay_order_id);
+    console.log("Payment ID:", razorpay_payment_id);
+    console.log("Received Signature:", razorpay_signature);
+    console.log("Expected Signature:", expectedSignature);
+    console.log("Secret exists:", !!process.env.RAZORPAY_KEY_SECRET);
+    console.log("======================================");
+    */
 
     const purchase = await CoursePurchase.findOne({
       paymentId: razorpay_order_id
     });
 
+
     if (!purchase) {
-      throw new ApiError(404, "Purchase record not found")
+
+      logPayments({
+        userId: req.user.id,
+        courseId: null,
+        orderId: razorpay_order_id,
+        paymentId: razorpay_payment_id,
+        amount: null,
+        currency: "INR",
+        status: "failed",
+        verificationTime: Date.now(),
+        failureReason: "Purchase record not found"
+      });
+      throw new ApiError(404,
+        "Purchase record not found"
+      )
     };
 
     purchase.status = "completed";
     await purchase.save();
 
-if(purchase.status === "completed"){
-  
-    try {
-      await sendEmail({
-        email:  req.user.email,
-        subject: "You’re in! Start your course today",
-        mailgenContent: PurchasedCourseMailgenContent(
-          req.user.name ,
 
-        )
-      });
-      console.log("REQ.email:", req.user.email)
-      console.log("Mail sent successfully.🎊")
-    } catch (error) {
-      console.log("Mail failed to send ")
-      console.error("error", error)
-      throw error
-    }
 
-    res.status(200)
-      .json({
-        success: true,
-        message: "Payment verified Successfully.",
-        courseId: purchase.course
-      });
+    logPayments({
+      userId: purchase.user,
+      courseId: purchase.courseId,
+      orderId: razorpay_order_id,
+      paymentId: razorpay_payment_id,
+      amount: purchase.price,
+      currency: purchase.currency,
+      status: "completed",
+      verificationTime: Date.now(),
+      failureReason: null
+    });
+
+
+    if (purchase.status === "completed") {
+
+      try {
+        await sendEmail({
+          email: req.user.email,
+          subject: "You’re in! Start your course today",
+          mailgenContent: PurchasedCourseMailgenContent(
+            req.user.name,
+
+          )
+        });
+        // debug
+        console.log("REQ.email:", req.user.email)
+        console.log("Mail sent successfully.🎊")
+
+      }
+      catch (error) {
+        console.log("Mail failed to send ")
+        console.error("error", error)
+        throw error
+      }
+      res.status(200)
+        .json({
+          success: true,
+          message: "Payment verified Successfully.",
+          courseId: purchase.course
+        });
     };
   } catch (error) {
     console.error("Error verifying payment", error);
@@ -166,7 +234,4 @@ if(purchase.status === "completed"){
         error: error.message
       });
   }
-
 };
-
-
---
+++ services/payment.logger.js
@@ -0,0 +1,53 @@
+import winston from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
+
+
+
+const paymentLogger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+
+
+    transports: [
+        new DailyRotateFile({
+            filename: "logs/Payments/payment-%DATE%.log",
+            datePattern: "YYYY-MM-DD",
+            maxFiles: "30d",
+            zippedArchive: false
+        })
+    ]
+
+});
+
+
+
+export const logPayments = ({
+    userId,
+    courseId,
+    orderId,
+    paymentId,
+    amount,
+    currency,
+    status,
+    failureReason
+
+
+}) => {
+    paymentLogger.info({
+        event: "PAYMENT",
+
+
+        userId,
+        courseId,
+        orderId,
+        paymentId,
+        amount,
+        currency,
+        status,
+        failureReason
+
+    });
+};
